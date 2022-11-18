8665f4fa6585fd7f69e283adc9ad5051c0bdd30a
Isai Oliva <i.oliva@uniandes.edu.co>
2022-11-18 13:26:13 -0400

    test rojo unitaria reporte

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const scenarioTemplate = require('./scenario_template.json');
 async function ejecutarScript() {
   
 
-  let listaEscenarios = [];
-  let escenarios = 2
-  let pasos = 2
+  let scenarios = [];
+  let escenarios = 1
+  let pasos = 1
 
   for (let escenario = 0; escenario < escenarios; escenario++){
     for (let paso = 0; paso < pasos; paso++) {
-      let nombreEscenario = `escenario_${escenario + 1}_step_${paso + 1}`;
+      let nombreEscenario = `esc_${escenario + 1}_step_${paso + 1}`;
       let pathReferencia = `./backstop_data/capturas/v3.42/v3.42_${nombreEscenario}.png`;
       let pathTest = `./backstop_data/capturas/v5.19.0/v5.19.0_${nombreEscenario}.png`;
-      listaEscenarios.push(
+      scenarios.push(
           {
             ...scenarioTemplate,
             "label": `Step:${paso + 1} de Escenario:${escenario + 1} `,
@@ -29,7 +29,7 @@ async function ejecutarScript() {
 
   let backstopJson = {
     ...backstopTemplate,
-    listaEscenarios
+    scenarios
   }
 
   fs.writeFileSync('backstop.json', JSON.stringify(backstopJson));
