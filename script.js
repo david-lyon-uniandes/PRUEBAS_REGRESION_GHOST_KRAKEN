17be80058faa9be0b31fc973910cc5449ea03550
Isai Oliva <i.oliva@uniandes.edu.co>
2022-11-18 13:52:44 -0400

    pruebas de backstop

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ async function ejecutarScript() {
   
 
   let scenarios = [];
-  let escenarios = 1
-  let pasos = 1
+  let escenarios = 4
+  let pasos = 7
 
   for (let escenario = 0; escenario < escenarios; escenario++){
     for (let paso = 0; paso < pasos; paso++) {
       let nombreEscenario = `esc_${escenario + 1}_step_${paso + 1}`;
       let pathReferencia = `./backstop_data/capturas/v3.42/v3.42_${nombreEscenario}.png`;
-      let pathTest = `./backstop_data/capturas/v5.19.0/v5.19.0_${nombreEscenario}.png`;
+      let pathTest = `./backstop_data/capturas/v5.19.0/v5.19_${nombreEscenario}.png`;
       scenarios.push(
           {
             ...scenarioTemplate,
